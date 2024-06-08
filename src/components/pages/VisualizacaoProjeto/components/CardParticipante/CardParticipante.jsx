58198e3dd818333e91c688a2b465f77be5c436fc
Riltter <riltterkennedy@gmail.com>
2024-06-08 19:47:50 -0300

    feat: ✨ add participant listing and user page redirection

--- src/components/pages/VisualizacaoProjeto/components/CardParticipante/CardParticipante.jsx
+++ src/components/pages/VisualizacaoProjeto/components/CardParticipante/CardParticipante.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import styles from "./CardParticipante.module.css";
+import { useNavigate } from "react-router-dom";
 
-const CardParticipante = ({ nome, faculdade, fotoUrl }) => {
+const CardParticipante = ({ id, nome, faculdade, fotoUrl }) => {
+  const navigate = useNavigate();
+
+  const navigateToProfile = (userid) => {
+    navigate(`/userprofile/${userid}`);
+  };
+
   return (
-    <div className={styles.card}>
+    <div className={styles.card} onClick={() => navigateToProfile(id)}>
       <img className={styles.fotoParticipante} src={fotoUrl} alt="pessoa-foto" />
       <div className={styles.userInfo}>
         <p className={styles.nome}>{nome}</p>
+        <p className={styles.faculdade}>{faculdade}</p>
       </div>
     </div>
   );
