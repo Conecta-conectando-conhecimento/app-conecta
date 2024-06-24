a9fa38f20409202a00fae563046f6f159c52b7f4
Thallyston <thallyston.schappo@gmail.com>
2024-06-23 21:19:25 -0300

    Acrescentado o import do Link

--- src/components/pages/ResetPassword/ResetPassword.jsx
+++ src/components/pages/ResetPassword/ResetPassword.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import style from './ResetPassword.module.css';
+import { Link } from 'react-router-dom';
 
 function ResetPassword() {
     const [cpf, setCpf] = useState('');
