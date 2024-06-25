6f9f0a615f2b9fdca3b0667aea07280f8be3f22a
Thallyston <thallyston.schappo@gmail.com>
2024-06-25 19:54:56 -0300

    aaa

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -1,3 +1,3 @@
-export const apiUrl = 'https://api-conecta-infy1xg3s-api-conecta.vercel.app';
-//export const apiUrl = 'http://localhost:8000';
+//export const apiUrl = 'https://api-conecta-infy1xg3s-api-conecta.vercel.app';
+export const apiUrl = 'http://localhost:8000';
 
